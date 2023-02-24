c4ea8511f4045ff0a5fc85a694c4f7167b9ae8e7
Dave Enrico <denricoNBHS@users.noreply.github.com>
2023-02-24 19:55:19 +0000

    Finish homework for 2/14

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 class Vector2d:
-    pass
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return f"Vector2d(x={self.x}, y={self.y})"
+    
+    def __str__(self):
+        return f"{self.x}i + {self.y}j"
+
+    def __abs__(self):
+        return (self.x ** 2 + self.y ** 2) ** .5
+    
