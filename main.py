54bdb81bfa77ca44bbabe94093149762b4379c56
nirmohithakur <121183908+nirmohithakur@users.noreply.github.com>
2023-02-24 00:27:22 +0000

    abs

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 class Vector2d:
-    pass
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+    def __repr__(self):
+        return f"Vector2d({self.x} = x, {self.y} = y)"
+    def __str__(self):
+        return f"{self.x}*i + {self.y}*j"
+    def __abs__(self):
+        return ((self.x)**2) + (self.y)**2))**(1/2))
